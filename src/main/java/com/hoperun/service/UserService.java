1c1c9dfdba464955e77727c178c394a5d5752674
wu_zhengxu <50321107+NoZuoNoDie99@users.noreply.github.com>
2019-07-29 16:59:49 +0800

    Initial commit

---
+++ src/main/java/com/hoperun/service/UserService.java
@@ -0,0 +1,12 @@
+package com.hoperun.service;
+
+import com.hoperun.pojo.User;
+
+/**
+ * @descriptiom:
+ * @Author: 武政旭
+ * @Date: 2019/7/29 15:48
+ */
+public interface UserService {
+    public User selectUser(long userId);
+}
